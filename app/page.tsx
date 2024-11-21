5b1ebe3553ffbc5ddf2086b9f9409caabdef5c05
Abinnovator <abtheinnovator@gmail.com>
2024-11-21 18:42:50 +0530

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      <div className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
+      <h1 className="h1-bold">Welcome to the world of Next.js(Inter)</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js (SpaceGrotesk)
+      </h1>
+      {/* <div className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
         Tailwind CSS is Fun
-      </div>
+      </div> */}
     </>
   );
 }
