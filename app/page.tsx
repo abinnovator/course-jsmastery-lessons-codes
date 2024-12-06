f4a97e43016191122dc2cbe51335f511735d0d74
Abinnovator <abtheinnovator@gmail.com>
2024-12-06 17:32:19 +0530

    Light & Dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Navbar from "@/components/navigation/navbar";
 
 export default function Home() {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js(Inter)</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js (SpaceGrotesk)
-      </h1>
+      <Navbar />
+      {/* <h1 className="h1-bold">Welcome to the world of Next.js</h1> */}
       {/* <div className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
         Tailwind CSS is Fun
       </div> */}
